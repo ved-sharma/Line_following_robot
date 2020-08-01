945cdf78e1edb8eeb2b4e47d52e08f5af53c81d7
Ved Sharma <vedsharma@gmail.com>
2020-07-31 23:55:20 -0400

    Update Line_tracking.ino
    version 03

--- Line_tracking.ino
+++ Line_tracking.ino
@@ -1,9 +1,6 @@
-// Ved P. Sharma, May 4, 2013, version 02
-// This version is for two wheels (+DC motors) in the center with support
-// at the front and back by two tmaiya caster wheels.
-// Relectance sensros in the middle aligning with the wheels was not 
-// not a good configuration. Had to move them at the front to get the
-// triangle configuration with 2 wheels and reflectance sensors
+// Ved P. Sharma, May 5, 2013, version 03
+// Robot follows black line, turns 180 deg and comes back
+// and follows the same line back
 
 #include <QTRSensors.h>
 #define NUM_SENSORS   2
@@ -11,13 +8,13 @@
 #define EMITTER_PIN   QTR_NO_EMITTER_PIN
 
 QTRSensorsRC qtrrc((unsigned char[]) {3, 4}, NUM_SENSORS, TIMEOUT, EMITTER_PIN);
-unsigned int sensorValues[NUM_SENSORS];
+unsigned int sensors[NUM_SENSORS];
 
 const int m1Pin1 = 5;  const int m1Pin2 = 6;
 const int m2Pin1 = 7;  const int m2Pin2 = 8;
 const int m1enablePin = 9;  const int m2enablePin = 10;
-int a = 0, b = 0;
-int wheelSpeed = 255, turnSpeed = 150, lower = 800, upper = 1400;
+int a = 0, b = 0, i=0;
+int wheelSpeed = 255, turnSpeed = 100, lower = 800, upper = 1400;
 
 void setup() {
   Serial.begin(9600);
@@ -27,8 +24,7 @@ void setup() {
   pinMode(m1enablePin, OUTPUT);  pinMode(m2enablePin, OUTPUT);
   do {
     delay(250);
-    qtrrc.read(sensorValues);
-    a = sensorValues[0];  b = sensorValues[1];
+    qtrrc.read(sensors);  a = sensors[0];  b = sensors[1];
     Serial.print(a);  Serial.print('\t');  Serial.print(b);  Serial.println();
   } while ((a<lower || a>upper) || (b<lower || b>upper));
 
@@ -39,16 +35,28 @@ void setup() {
 }
 
 void loop() {
-  delay(100);
-  qtrrc.read(sensorValues);
-  a = sensorValues[0];  b = sensorValues[1];
+  for(i=0;i<300;i++){
+    check();
+    delay(20);
+  }
+  turn();
+  for(i=0;i<300;i++){
+    check();
+    delay(20);
+  }
+  turn();
+//  analogWrite(m1enablePin, 0);  analogWrite(m2enablePin, 0); 
+//  delay(1000);
+//  analogWrite(m1enablePin, wheelSpeed);  analogWrite(m2enablePin, wheelSpeed); 
+}
+
+void check() {
+  qtrrc.read(sensors);  a = sensors[0];  b = sensors[1];
   Serial.print(a);  Serial.print('\t');  Serial.print(b);  Serial.println();
   if(a<lower && b>lower && b<upper){
     do {
-//      delay(10);
       analogWrite (m2enablePin, 0);
-//      analogWrite (m1enablePin, turnSpeed);
-      qtrrc.read(sensorValues);a = sensorValues[0];  b = sensorValues[1];
+      qtrrc.read(sensors);a = sensors[0];  b = sensors[1];
       Serial.print(a);  Serial.print('\t');  Serial.print(b);  Serial.println();
     }
     while (a<lower);
@@ -56,13 +64,25 @@ void loop() {
   }
   else if (b<lower && a>lower && a<upper){
     do {
-//      delay(10);
       analogWrite (m1enablePin, 0);
-//      analogWrite (m2enablePin, turnSpeed);
-      qtrrc.read(sensorValues);a = sensorValues[0];  b = sensorValues[1];
+      qtrrc.read(sensors);a = sensors[0];  b = sensors[1];
       Serial.print(a);  Serial.print('\t');  Serial.print(b);  Serial.println();
     }
     while (b<lower);
     analogWrite (m1enablePin, wheelSpeed);
   }
 }
+
+void turn(){
+  analogWrite(m1enablePin, turnSpeed);  analogWrite(m2enablePin, turnSpeed); 
+  digitalWrite(m1Pin1, LOW); digitalWrite(m1Pin2, HIGH);
+  delay(300); // this delay imp for robot to move out of the line
+  do {
+    qtrrc.read(sensors);a = sensors[0];  b = sensors[1];
+    Serial.print(a);  Serial.print('\t');  Serial.print(b);  Serial.println();
+  }
+  while (a<lower || b<lower);
+  digitalWrite(m1Pin1, HIGH); digitalWrite(m1Pin2, LOW); 
+  analogWrite(m1enablePin, wheelSpeed);  analogWrite(m2enablePin, wheelSpeed); 
+}
+
